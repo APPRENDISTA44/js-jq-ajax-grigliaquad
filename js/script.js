0191d82423ee11cebbecf9abab3316bd8b8e36e5
Luca Isidoro <apprendista44@gmail.com>
2020-06-25 16:35:55 +0200

    esercizio completato con handlebars

---
+++ js/script.js
@@ -0,0 +1,68 @@
+$(document).ready(function () {
+
+  var numeroQuadrati = 36
+  for (var i = 0; i < numeroQuadrati ; i++) {
+    var source = $('#quadrato-template').html();
+    var template = Handlebars.compile(source);
+    var html = template();
+    $('.container').append(html);
+  }
+
+  $(document).on('click','.entry',
+  function () {
+    var elementoCliccato = this;
+    $.ajax(
+      {
+        url: 'https://flynn.boolean.careers/exercises/api/random/int',
+        method: 'GET',
+        success: function(data) {
+          $(elementoCliccato).removeClass('yellow');
+          $(elementoCliccato).removeClass('green');
+          var numero = data.response;
+          $(elementoCliccato).children('h2').text(numero);
+          if (numero<6) {
+              $(elementoCliccato).addClass('yellow');
+          }else {
+            $(elementoCliccato).addClass('green');
+          }
+        },
+        error: function() {
+            alert("Si è verificato un errore");
+        }
+
+      }
+    );
+
+  });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
